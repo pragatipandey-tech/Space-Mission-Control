f2c1cae1553d000c903e606db6720400eef6a5ff
pragatipandey-tech <pragatipandey.ds@gmail.com>
2026-08-06 22:33:08 +0530

    refactor: render launch cards using map

--- frontend/src/pages/Launches.tsx
+++ frontend/src/pages/Launches.tsx
@@ -1,5 +1,6 @@
 import PageHeader from "../components/common/PageHeader";
 import LaunchCard from "../components/launches/LaunchCard";
+import { launches } from "../data/launches";
 
 export default function Launches() {
   return (
@@ -16,26 +17,15 @@ export default function Launches() {
       />
 
       <div className="grid grid-cols-1 gap-6 lg:grid-cols-2">
-        <LaunchCard
-          mission="Artemis II"
-          rocket="SLS"
-          date="2026"
-          status="Scheduled"
-        />
-
-        <LaunchCard
-          mission="Starlink 58"
-          rocket="Falcon 9"
-          date="August 2026"
-          status="Ready"
-        />
-
-        <LaunchCard
-          mission="Gaganyaan"
-          rocket="LVM3"
-          date="2026"
-          status="Preparing"
-        />
+        {launches.map((launch) => (
+          <LaunchCard
+           key={launch.mission}
+           mission={launch.mission}
+           rocket={launch.rocket}
+           date={launch.date}
+           status={launch.status}
+          />
+        ))}
       </div>
     </div>
   );
